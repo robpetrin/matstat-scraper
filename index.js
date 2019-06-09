5526ae7186f16a87d09c41af97d7bdfb675b3580
Rob Petrin <robpetrin@gmail.com>
2019-06-09 08:51:26 -0400

    get list of pages

--- index.js
+++ index.js
@@ -2,18 +2,13 @@
 const tabletojson = require('tabletojson');
 const fs = require('fs');
 const json2csv = require('json2csv').parse;
-var urlBase = "http://www.profightdb.com/atoz.html?term=";
-var urlMid = "&start=";
+var urlBase = "http://profightdb.com/atoz.html?term=z&start=0";
 
-tabletojson.convertUrl(,
+tabletojson.convertUrl(fullURL,
     function(tablesAsJson) {
-        var tableParsed = tablesAsJson[1];
-        tableParsed.forEach(function(v) {
-            delete v["Expand All"],
-                delete v["Web Description"]
-        });
-        console.log(tableParsed[1]);
-        fs.writeFile("output.json", JSON.stringify(tablesAsJson[1], null, "\t"), function(err) {
+        var tableParsed = tablesAsJson[0];
+        console.log(tableParsed);
+        fs.writeFile("output.json", JSON.stringify(tablesAsJson[0], null, "\t"), function(err) {
             if (err) {
                 console.log("An error occured while writing JSON Object to File.");
                 return console.log(err);
@@ -21,9 +16,9 @@ tabletojson.convertUrl(,
             console.log("JSON file has been saved.");
         });
 
-        const csv = json2csv(tablesAsJson[1], ['Name of Covered Entity', 'State', 'Covered Entity Type', 'Individuals Affected', 'Breach Submission Date', 'Type of Breach', 'Location of Breached Information', 'Business Associate Present']);
+        const csv = json2csv(tablesAsJson[0]);
 
-        fs.writeFile('output.csv', csv, function(err) {
+        fs.writeFile('./output.csv', csv, function(err) {
             if (err) throw err;
             console.log('CSV Saved.');
         });
--- profightdb.js
+++ profightdb.js
@@ -4,43 +4,17 @@ const fs = require('fs');
 const json2csv = require('json2csv').parse;
 var urlBase = "http://www.profightdb.com/atoz.html?term=";
 var urlMid = "&start=";
+var letters = [];
+    for (var i = 97; i <= 122; i++) { letters.push(String.fromCodePoint(i)) }
+var linkList = [];
 
-const letters = (() => {
-    const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
-    return caps.concat(caps.map(letter => letter.toLowerCase()));
-})();
-
-letters.forEach(function(value) {
-    for (let i = 0; i < 2200; i += 100) {
-        console.log(i);
-
-        // tabletojson.convertUrl(URL,
-        //     function(tablesAsJson) {
-        //         var tableParsed = tablesAsJson[1];
-        //         tableParsed.forEach(function(v) {
-        //             delete v["Expand All"],
-        //                 delete v["Web Description"]
-        //         });
-        //         console.log(tableParsed[1]);
-        //         fs.writeFile("output.json", JSON.stringify(tablesAsJson[1], null, "\t"), function(err) {
-        //             if (err) {
-        //                 console.log("An error occured while writing JSON Object to File.");
-        //                 return console.log(err);
-        //             }
-        //             console.log("JSON file has been saved.");
-        //         });
-
-        //         const csv = json2csv(tablesAsJson[1], ['Name of Covered Entity', 'State', 'Covered Entity Type', 'Individuals Affected', 'Breach Submission Date', 'Type of Breach', 'Location of Breached Information', 'Business Associate Present']);
-
-        //         fs.writeFile('output.csv', csv, function(err) {
-        //             if (err) throw err;
-        //             console.log('CSV Saved.');
-        //         });
-        //     }
-        // );
-
-
-
-
+letters.forEach(function(v) {
+    for (let i = 0; i < 22; i++) {
+        var fullURL = urlBase + v + urlMid + (i * 100).toString(); 
+        linkList.push(fullURL);
     }
+});
+
+linkList.forEach(function(v) {
+    console.log(v);
 });
